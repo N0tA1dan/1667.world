c5205299b9139a493439fc2c51e8ec03358b973a
Aidan <71911942+N0tA1dan@users.noreply.github.com>
2023-01-20 10:29:08 -0800

    Update and rename main.js to sparkle.js

--- sparkle.js
+++ sparkle.js
@@ -1,11 +1,3 @@
-var splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click',()=>{
-  splashScreen.style.opacity = 0;
-  setTimeout(()=>{
-    splashScreen.classList.add('hidden')
-  },610)
-})
-
 // <![CDATA[
 var colour = "#FFFFFF";
 var sparkles = 120;
